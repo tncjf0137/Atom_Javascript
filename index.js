d08a6357c86204b395a1adf95eec90f98e8c687c
tncjf0137 <tncjf0137@gmail.com>
2020-08-19 17:02:24 +0900

    if&&else

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-const calculator={
-  plus:function(a,b){return a+b;},
-  minus:function(a,b){return a-b;},
-  multiple:function(a,b){return a*b;},
-  divide:function(a,b){return a/b;}
+const age = prompt("How old are you");
 
+if(age>18){
+console.log("you can drink");
 
+}else{
+  console.log("you cant");
 }
-console.log(calculator.plus(5,5))
-console.log(calculator.minus(5,5))
-console.log(calculator.multiple(5,5))
-console.log(calculator.divide(5,5))
-
-function sayhello(name,age){
-console.log('hello ${name} you are ${age} years old');
-}
-sayhello("soocheol",15);
-
-const title=document.getElementById('title');
-
-console.log(title);
-title.innerHTML ="hi fr"
-console.log(title);
-title.style.color="red";
