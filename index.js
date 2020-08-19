4d34ebdb913a0931f2328c654a99c37597cdae5f
tncjf0137 <tncjf0137@gmail.com>
2020-08-19 12:33:24 +0900

    Using_Atom

---
+++ index.js
@@ -0,0 +1,23 @@
+const calculator={
+  plus:function(a,b){return a+b;},
+  minus:function(a,b){return a-b;},
+  multiple:function(a,b){return a*b;},
+  divide:function(a,b){return a/b;}
+
+
+}
+console.log(calculator.plus(5,5))
+console.log(calculator.minus(5,5))
+console.log(calculator.multiple(5,5))
+console.log(calculator.divide(5,5))
+
+function sayhello(name,age){
+console.log('hello ${name} you are ${age} years old');
+}
+sayhello("soocheol",15);
+
+const title=document.getElementById('title');
+
+console.log(title);
+title.innerHTML ="hi fr"
+console.log(title);
