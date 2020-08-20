ecbb1be5653b6b370520f5feac8adac12b25eab9
tncjf0137 <tncjf0137@gmail.com>
2020-08-20 15:11:02 +0900

    ternary operator(삼항 연산자)

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ function getTime(){
   const date=new Date();
   const minutes=date.getMinutes();
   const hours=date.getHours();
-  clockTitle.innerText=`${hours}:${minutes}`;
+  const seconds=date.getSeconds();
+
+  clockTitle.innerText=`${hours<10 ? `0${hours}`:hours}:${minutes<10 ? `0${minutes}`:getMinutes}
+:${seconds<10 ? `0${seconds}`:seconds}`;
 }
 
 
 function init(){
   getTime();
+  setInterval(getTime,1);
 }
 init();
