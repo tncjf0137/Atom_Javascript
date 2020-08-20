738124204e00a036d46d1f0347efca28862665cd
tncjf0137 <tncjf0137@gmail.com>
2020-08-20 13:56:35 +0900

    dom practice 2

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const title=document.querySelector("#title");
 
-const BASE_COLOR="#8e44ad";
-const OTHER_COLOR="#2980b9";
+const CLICKED_CLASS="clicked";
 
 function handleClick(){
-  const currentColor=title.style.color;
-  if(currentColor===BASE_COLOR){
-    title.style.color=OTHER_COLOR;
-  }else{title.style.color=BASE_COLOR;
+title.classList.toggle(CLICKED_CLASS);
+
 }
-}
+
 function init(){
-title.style.color= BASE_COLOR;
-title.addEventListener("click",handleClick);
-
-
+  title.addEventListener("click",handleClick);
 }
 init();
