bfe453c64ee8afe98df9f14f9b11c4567081e44c
tncjf0137 <tncjf0137@gmail.com>
2020-08-22 14:22:50 +0900

    name2

--- gretting.js
+++ gretting.js
@@ -8,10 +8,26 @@ function paintGreeting(text){
   greeting.classList.add(showing);
 greeting.innerText=`Hello '${text}'`
 }
+function saveName(text){
+localStorage.setItem(user,text);
+
+
+
+}
+function handleSubmit(event){
+event.preventDefault();
+const currentValue=input.value;
+paintGreeting(currentValue);
+saveName(currentValue);
+}
+function askForName(){
+  form.classList.add(showing);
+  form.addEventListener("submit",handleSubmit);
+}
  function loadName(){
 const currentUser=localStorage.getItem(user);
 if(currentUser===null){
-
+askForName();
 }else{
 paintGreeting(currentUser);
 
